303096e221e55e5d59cd2423854dcb7cdf3d5219
Lukas Parsons <Lukripar@gmail.com>
2019-02-18 09:14:02 -0500

    tweaked some css.
    I also added a "CollapseHellper" class that I may or may not use to uncollapse the first project in a list of projects.

---
+++ LPBugTracker/Helpers/CollapseHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LPBugTracker.Helpers
+{
+    public class CollapseHelper
+    {
+        public static string uncollapseFirst(int count)
+        {
+            var result = "";
+            if (count != 1)
+            {
+                result = "collapsed";
+            }
+            return result;
+        }
+    }
+}
